a19d3a26f034734c87578ed442fcf2df460883a8
Gustavo dos Santos <gustavods2003@outlook.com>
2022-12-19 08:58:27 -0300

    Validação de Email

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,9 +1,40 @@
+const Knex = require("knex")
+const Email = require('../services/Email')
+
 class UserController{
 
   async indexedDB(req, res){}
 
   async create (req, res){
-    res.send(req.body)
+    const { nome, password, email } = req.body
+
+    if(!password)
+      return res.status(400).send({erro: 'Insira uma Senha!'})
+    if(!email) 
+      return res.status(400).send({erro: 'Insira um Email!'})
+    if(!nome) 
+      return res.status(400).send({erro: 'Insira um Email!'})
+
+    const validacaoEmail = Email.ValidarEmail(email)
+
+    console.log(validacaoEmail)
+
+    if(!validacaoEmail)
+      return res.status(400).send({erro: 'Email Inválido'})
+    
+    try {
+      // const exist = await Knex('users').select().where({email})
+      // if(exist)
+      //   return res.status(400).send({erro: 'Email ja cadastrado'})
+
+      // await Knex('users').insert({
+      //   email,
+      //   password
+      // })
+      return res.status(200).send({message: 'Usuário cadastrado com sucesso'})
+    }catch(err){
+      res.status(400).send(err)
+    }
   }
 }
 
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
-const app = express()
 const cors = require('cors')
+
+const app = express()
 const router = require("./routes/routes")
  
 app.use(cors())
